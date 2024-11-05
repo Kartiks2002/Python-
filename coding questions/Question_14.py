eee8ada24bc4bb414a876ce1dbe4ab962c94a9e2
Kartiks2002 <colabkartik24@gmail.com>
2024-11-06 01:31:54 +0530

    Delete coding questions/Question_14.py

--- coding questions/Question_14.py
+++
@@ -1,6 +0,0 @@
-# 14.	Given a list and find even numbers in that list using lambda function
-
-lis = [1,2,3,4,5,6]
-# even_nos = [i for i in lis if i%2 == 0]
-even_nos = list(filter(lambda x: x % 2 == 0, lis))
-print(even_nos)
